3316533b2bfbf5982591042f58535055813325ce
Vladislav Gladkiy <ledniy@gmail.com>
2015-05-17 00:33:29 +0300

    разделил js, немного рефактора

--- app/js/main.js
+++ app/js/main.js
@@ -1,112 +1,11 @@
-var dragDrop = (function(){
-
-        var init = function(){
-                _setUpListeners;
-            },
-
-            _setUpListeners = function(){
-                //listeners
-            },
-
-            _appendDraggableEl = function(obj){
-                var xpos = $('.b-controls input[name="xpos"]'),
-                    ypos = $('.b-controls input[name="ypos"]'),
-                    image = $('.b-main-area');
-
-                obj.appendTo(image).draggable({
-                    containment: image,
-                    cursor: "move",
-                    drag: function( event, ui ) {
-                        xpos.val(ui.position.left);
-                        ypos.val(ui.position.top);
-                    }
-                });
-            },
-            _resetPosition = function(){
-                var xpos = $('.b-controls input[name="xpos"]'),
-                    ypos = $('.b-controls input[name="ypos"]'),
-                    wtmark = $('#watermark');
-
-                xpos.val(0);
-                ypos.val(0);
-                wtmark.css({
-                    'left': 0,
-                    'top': 0
-                })
-            };
-
-        return {
-            init: init,
-            appendEl: _appendDraggableEl,
-            reset: _resetPosition
-        };
-})();
-
-var wmarkOpacity = (function(){
-
-    var init = function(){
-        var sliderEl = $('.b-opacity-slider');
-        var wtmark = $('.b-main-area .drag');
-            if (sliderEl.length) {
-                 sliderEl.slider({
-                      min: 1,
-                      max: 100,
-                      value: 100,
-                      range: "min",
-                      slide: function(event, ui) {
-                          wtmark.css('opacity', ui.value / 100);
-                      }
-                 });
-            }
-        },
-        _resetOpacity = function(){
-            var sliderEl = $('.b-opacity-slider');
-            var wtmark = $('.b-main-area .drag');
-                sliderEl.slider("value", 100);
-                wtmark.css('opacity', 100);
-
-        };
-
-    return {
-        init: init,
-        reset: _resetOpacity
-    };
-})();
-
 $(document).ready(function(){
-    dragDrop.init();
-    dragDrop.appendEl($('<div id="watermark" style="width: 20px; border: 1px solid red;" class="drag">Drag me </div>'));
     wmarkOpacity.init();
+    upload.init();
 
     $('.m-btns :reset').on('click', function(){
         dragDrop.reset();
         wmarkOpacity.reset();
     });
-
-    //fileupload basic https://github.com/blueimp/jQuery-File-Upload/wiki/Basic-plugin
-  $(function () {
-      $('#fileupload').fileupload({
-          dataType: 'json',
-          done: function (e, data) {
-              $.each(data.result.files, function (index, file) {
-                  var upploadedImage = $('<div id=uploaded-image class=b-main-area__uploaded-image></div>');
-                  $('#workspace').prepend(upploadedImage);
-                  $(upploadedImage).css('background-image', 'url(files/' + file.name + ')');
-              });
-          }
-      });
-  });
-  $(function () {
-      $('#wmarkfile').fileupload({
-          dataType: 'json',
-          done: function (e, data) {
-              $.each(data.result.files, function (index, file) {
-                  $('#watermark').css('background', 'url(files/'+file.name+') no-repeat');
-              });
-          }
-      });
-  });
-
 });
 
 
---
+++ app/js/upload.js
@@ -0,0 +1,27 @@
+var upload = (function(){
+
+    var init = function(){
+        $('input[type="file"]').fileupload(
+            {
+                dataType: 'json',
+                done: function (e, data) {
+                    var type = $(this).is("#fileupload") ? 'image' : 'watermark',
+                        imageName = data.result.files[0].name,
+                        src = "files/" + imageName,
+                        image = $('<img id="'+ type +'" src="' + src + '" class="b-main-area-' + type + '">');
+
+                    if(type == 'image'){
+                        $('#workspace').append(image);
+                    }
+                    else{
+                        dragDrop.appendEl(src);
+                    }
+
+                }
+            });
+    };
+
+    return {
+        init: init
+    }
+})();
