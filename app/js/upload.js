1b8ccbbbb6d78250767cb63283c8bde210aa9745
Maxim Pogrebnoy <maxlanser@gmail.com>
2015-05-23 13:00:33 +0300

    .m-disabled до выбора вотермарка

--- app/js/upload.js
+++ app/js/upload.js
@@ -53,6 +53,7 @@ var upload = (function(){
 			wmarkOpacity.enable();
 			wmarkPosition.init();
 			$('.m-disabled-area').css('display','none');
+			$('.b-section').removeClass('m-disabled');
 			$('.m-btns input').prop('disabled', false);
 		}
 		$('#wmarkfile').prop('disabled', false);
