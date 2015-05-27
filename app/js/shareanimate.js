5aa97736fc9028b346b2d84abbaed33c55deac71
irina.tishkevich <irina.tishkevich@viaden.com>
2015-05-27 14:40:15 +0300

    Updated share js; Removed unnecessary images; Renamed sprite images;

--- app/js/shareanimate.js
+++ app/js/shareanimate.js
@@ -1,72 +1,61 @@
-	$('.b-share-icon').on('click', function(){
-		var e = $('.b-share');
-		if (e.hasClass('m-opened')) {
-			$('.b-share-icon').animate(
-				{
-					'right':'0px'
-				},
-				{
-					duration: 200
-				}
-			);
-			$('.b-share-socials-list').animate(
-				{
-					'left':'-43px'
-				}, 
-				{
-					duration: 200,
-					complete: function(){
-						e.removeClass('m-opened');
-					}
-				}
-			);
+var share = (function(){
 
+	// set variables
+	var speed = 200,
+		share = $('.b-share'),
+		shareIcon = $('.b-share-icon'),
+		shareList = $('.b-share-socials-list');
+
+	var init = function(){
+		_setupListeners();
+	},
+
+	// set listeners
+	_setupListeners = function(){
+		shareIcon.on('click', _shareToggle);
+		$(document).on('click', _shareHide);
+	},
+
+	// share toggle
+	_shareToggle = function(){
+		if (share.hasClass('m-opened')) {
+			_shareHideAnimation();
 		} else {
-			$('.b-share-icon').animate(
-				{
-					'right':'-43px'
-				},
-				{
-					duration: 200
-				}
-			);
-			$('.b-share-socials-list').animate(
-				{
-					'left':'0px'
-				},
-				{
-					duration: 200,
-					complete: function(){
-						e.addClass('m-opened');
-					}
-				}
-			);
+			_shareOpenAnimation();
 		}
-	});
+	},
 
-	$(document).click(function(event) { 
-	    if(!$(event.target).closest('.b-share-socials-list').length) {
-	        var e = $('.b-share');
-			if (e.hasClass('m-opened')) {
-				$('.b-share-icon').animate(
-					{
-						'right':'0px'
-					},
-					{
-						duration: 200
-					}
-				);
-				$('.b-share-socials-list').animate(
-					{
-						'left':'-43px'
-					},
-					{
-						duration: 200,
-						complete: function(){
-							e.removeClass('m-opened');
-						}
-					}
-				);
-			}  
-		}      
-	});
+	// share open
+	_shareOpenAnimation = function(){
+		shareIcon.animate({'right':'-43px'}, speed);
+		shareList.animate({'left':'0'}, speed,
+			function(){
+				share.addClass('m-opened');
+			}
+		);
+	},
+
+	// share hide
+	_shareHideAnimation = function(){
+		shareIcon.animate({'right':'0'}, speed);
+		shareList.animate({'left':'-43px'}, speed,
+			function(){
+				share.removeClass('m-opened');
+			}
+		);
+	},
+
+	// share hide by click on the document
+	_shareHide = function(){
+		if(!$(event.target).closest(shareList).length) {
+			if (share.hasClass('m-opened')) {
+				_shareHideAnimation();
+			}
+		}
+	};
+
+	return {
+		init: init
+	};
+
+})();
