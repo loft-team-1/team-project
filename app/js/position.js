464f839a525fdb975a636c2b40342744f4cd342f
irina.tishkevich <irina.tishkevich@viaden.com>
2015-05-28 23:52:07 +0300

    Fixed multi position;

--- app/js/position.js
+++ app/js/position.js
@@ -94,11 +94,11 @@ var wmarkPosition = (function(){
 
 		if(multi.hasClass('m-active')){
 			wmark.css(margin, $this.val() + 'px');
-			wmarkWrap.css(wh, (wmark.width() * clones) + parseInt($this.val()) * clones + 'px');
+			wmarkWrap.css('width', (wmark.width() * clones) + parseInt($this.val()) * clones + 'px');
 			if ($this.val() > 0) {
-				$('.b-interval' + hv).css(wh, Math.ceil($this.val()/2) + 'px');
+				$('.b-interval' + wh).css(hv, Math.ceil($this.val()/2) + 'px');
 			} else if ($this.val() == 0) {
-				$('.b-interval' + hv).css(wh, '1px');
+				$('.b-interval' + wh).css(hv, '1px');
 			}
 		} else {
 			wmarkWrap.css(axis, $this.val() + 'px');
@@ -135,9 +135,9 @@ var wmarkPosition = (function(){
 
 		if(multi.hasClass('m-active')){
 			wmark.css(margin, changeVal + 'px');
-			wmarkWrap.css(wh, (wmark.width() * clones) + changeVal * clones + 'px');
+			wmarkWrap.css('width', (wmark.width() * clones) + changeVal * clones + 'px');
 			if (changeVal > 0) {
-				$('.b-interval' + hv).css(wh, Math.ceil(changeVal/2) + 'px');
+				$('.b-interval' + wh).css(hv, Math.ceil(changeVal/2) + 'px');
 			}
 		} else {
 			wmarkWrap.css(axis, changeVal + 'px');
